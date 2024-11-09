5c32784b38f05f18f845118057e9ce12a33fb15c
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-11-09 15:42:31 +0200

    s1

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,36 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const fetchImages = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getPhotos(query, page);
+        console.log(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page, query]);
+  const handlSubmit = value => {
+    setQuery(value);
+  };
+
   return (
     <>
+      <Form onSubmit={handlSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
